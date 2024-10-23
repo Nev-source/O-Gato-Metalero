b5a334ef9e4b60ea994399bda5115ee238e6ef3b
Nev-source <ruanv.santiago@gmail.com>
2024-10-23 11:07:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,14 +28,14 @@ backgroundVideo.addEventListener('canplay', function() {
 
 function increaseDifficulty() {
   // Diminui o intervalo de criação de notas até um limite mínimo
-  if (noteCreationInterval > 300) {
+  if (noteCreationInterval > 260) {
     noteCreationInterval -= 80; // Diminui o tempo entre as notas
     clearInterval(noteInterval);
     noteInterval = setInterval(createNote, noteCreationInterval);
   }
 
   // Aumenta a velocidade das notas até um limite máximo
-  if (noteSpeed < 25) {
+  if (noteSpeed < 27) {
     noteSpeed += 1.5; // Aumenta a velocidade de movimento das notas
   }
 }
